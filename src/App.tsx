b4c3c2b69f992425bb53bbfb6b8da32f0d9b1449
Dana Popa <popa.dana100@gmail.com>
2024-04-07 15:44:41 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>Hello, world!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
